9fe6da20eaa1ff8c627ba1a2cb8f82ebf2d1f597
mingyu210 <163110795+mingyu210@users.noreply.github.com>
2025-06-25 16:08:08 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N;
+int dp[1001][3];
+int answer;
+
+int main() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < 3; j++) {
+			int a;
+			cin >> a;
+			dp[i][j] = a;
+		}
+	}
+
+	for (int i = 1; i < N; i++) {
+		dp[i][0] += min(dp[i - 1][1], dp[i - 1][2]);
+		dp[i][1] += min(dp[i - 1][0], dp[i - 1][2]);
+		dp[i][2] += min(dp[i - 1][0], dp[i - 1][1]);
+	}
+	
+	answer = min({dp[N - 1][0], dp[N - 1][1], dp[N - 1][2]});
+
+	cout << answer;
+	
+	return 0;
+	
+}
